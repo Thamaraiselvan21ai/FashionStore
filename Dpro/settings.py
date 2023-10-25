b21ff0e7413a8ac304fe0da285a7a48293a1ee6f
Thamaraiselvan21ai <139037933+Thamaraiselvan21ai@users.noreply.github.com>
2023-10-25 21:50:29 +0530

    Update settings.py

--- Dpro/settings.py
+++ Dpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@8^&+vm$qp(i3s!!)=n=q37i+3lk66qu_buy-xm25msdtz=&lj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
